ba554b44374fdde827ef948934e053c13508fc24
Ben Timby <btimby@gmail.com>
2022-02-14 13:23:48 -0500

    Add port scan.

--- back/api/views/hosts.py
+++ back/api/views/hosts.py
@@ -1,10 +1,12 @@
+import socket
 import logging
 
 import docker
 
 from restless.preparers import FieldsPreparer
+from restless.resources import skip_prepare
 
-from api.views import BaseResource
+from api.views import BaseResource, abort
 from api.config import DOCKER_SOCKET_PATH
 
 
@@ -33,6 +35,29 @@ def _container_details(container):
     }
 
 
+def _sniff(s, host):
+    # Try to determine protocol.
+    try:
+        s.send(f'HTTP/1.1 GET /\r\nHost: {host}\r\n'.encode())
+        reply = s.recv(128)
+
+    except socket.error:
+        return
+
+    if not reply:
+        return
+
+    reply = reply.split(b'\r\n')[0].lower()
+    LOGGER.debug('Received: %s', reply)
+
+    if reply.startswith(b'http'):
+        return 'http'
+    elif b'smtp' in reply:
+        return 'smtp'
+    elif b'ssh' in reply:
+        return 'ssh'
+
+
 class HostResource(BaseResource):
     "Manage docker hosts."
     preparer = FieldsPreparer(fields={
@@ -42,6 +67,9 @@ class HostResource(BaseResource):
         'ports': 'ports',
         'aliases': 'aliases',
     })
+    extra_actions = {
+        'port_scan': ['POST']
+    }
 
     def list(self):
         "List potential docker hosts/ports."
@@ -54,3 +82,37 @@ class HostResource(BaseResource):
         "Get container detail."
         d = docker.DockerClient(base_url=f'unix://{DOCKER_SOCKET_PATH}')
         return _container_details(d.containers.get(pk))
+
+    @skip_prepare
+    def port_scan():
+        try:
+            host = request.data['host']
+        except KeyError as e:
+            abort(400, {e.args[0]: 'is required'})
+
+        try:
+            ports = map(int, request.data.getlist('ports'))
+        except ValueError:
+            abort(400, {'ports': 'should be a list of integers'})
+
+        if not ports:
+            abort(400, {'ports': 'is required'})
+
+        results = {}
+
+        for port in ports:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(3)
+
+            try:
+                s.connect((host, port))
+                LOGGER.debug('Connected to %s:%i', host, port)
+                results[f'{host}:{port}'] = _sniff(s, host)
+
+            except socket.error as e:
+                continue
+
+            finally:
+                s.close()
+
+        return results
